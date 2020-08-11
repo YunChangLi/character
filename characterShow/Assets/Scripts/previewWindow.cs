7d81b7a5553e4f435a5c517fa60e0e1f897a5ff4
Bruce <bruce6531@gmail.com>
2020-08-11 09:59:19 +0800

    add 30 model and Editor window

---
+++ characterShow/Assets/Scripts/previewWindow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+//[CustomEditor(typeof(preview))]
+public class previewWindow : Editor
+{
+    public override bool HasPreviewGUI()
+    {
+        return true;
+    }
+    public override GUIContent GetPreviewTitle()
+    {
+        return new GUIContent("designPlayer");
+    }
+}
