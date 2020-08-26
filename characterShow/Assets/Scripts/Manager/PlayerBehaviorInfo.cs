50b4bbab5aa0d91dc2a4715a1b85b0b24697693b
Bruce <bruce6531@gmail.com>
2020-08-26 23:06:14 +0800

    add the movement vector in playerbehavior

--- characterShow/Assets/Scripts/Manager/PlayerBehaviorInfo.cs
+++ characterShow/Assets/Scripts/Manager/PlayerBehaviorInfo.cs
@@ -8,6 +8,17 @@ public class PlayerBehaviorInfo : BehaviorInfo
     public float MP { get; set; }
     //最大血量
     public float MaxMp ;
+    //內部調整的位移向量
+    private Vector3 movement;
+
+    public Vector3 MovementDir 
+    {
+        get 
+        {
+            if (IsBlocked) return Vector3.zero;
+            else return movement;
+        }
+    }
 
     /// <summary>
     /// player 各種狀態
@@ -15,6 +26,11 @@ public class PlayerBehaviorInfo : BehaviorInfo
     public bool CanAttack
     {
         get { return !IsBlocked && IsGrounded && Input.GetButtonDown("Fire1"); }
+    }
+    private void Update()
+    {
+        movement.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
     }
 
 
