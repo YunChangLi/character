f2a5e71d44ba9ce650b3db82e8fb73ddec637249
Bruce <bruce6531@gmail.com>
2020-08-28 01:25:52 +0800

    adjust the attack animator

--- characterShow/Assets/Scripts/Manager/PlayerBehaviorInfo.cs
+++ characterShow/Assets/Scripts/Manager/PlayerBehaviorInfo.cs
@@ -8,9 +8,7 @@ public class PlayerBehaviorInfo : BehaviorInfo
     public float MP { get; set; }
     //最大血量
     public float MaxMp ;
-    //內部調整的位移向量
-    private Vector3 movement;
-
+    
     public bool JumpInput;
 
     public Vector3 MovementDir 
@@ -27,16 +25,40 @@ public class PlayerBehaviorInfo : BehaviorInfo
     /// </summary>
     public bool CanAttack
     {
-        get { return !IsBlocked /*&& IsGrounded */&& Input.GetButtonDown("Fire1"); }
+        get { return !IsBlocked /*&& IsGrounded */ && isAttack; }
     }
     public bool IsDefense
     {
         get { return /**IsGrounded**/Input.GetKey(KeyCode.Z); }
     }
+
+    //內部調整的位移向量
+    private Vector3 movement;
+    //內部調整攻擊判定
+    private bool isAttack;
+    //用來延遲攻擊判定
+    private Coroutine AttackWait;
+    //延遲多少秒
+    const float AttackWaitTime = 0.1f;
+
     private void Update()
     {
         movement.Set(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+
         JumpInput = Input.GetButton("Jump");
+
+        if (Input.GetButtonDown("Fire1")) {
+
+            if (AttackWait != null)
+                StopCoroutine(AttackWait);
+            AttackWait = StartCoroutine(AttackWaitCoroutine());
+        }
+    }
+    private IEnumerator AttackWaitCoroutine() 
+    {
+        isAttack = true;
+        yield return new WaitForSeconds(AttackWaitTime);
+        isAttack = false;
     }
 
 
