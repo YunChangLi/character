067dd4503786c04ac4da7fc042f45984e1e1c644
Yen <pnf4665jds@gmail.com>
2020-10-25 00:26:46 +0800

    Create FireBall Effect

---
+++ characterShow/Assets/Scripts/Monster/Skills/FireBallMove.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireBallMove : MonoBehaviour
+{
+    public float Speed;
+    public GameObject ImpactPrefab;
+    public List<GameObject> Trails;
+
+    private Rigidbody rigid;
+
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if(Speed != 0 && rigid != null)
+        {
+            rigid.position += transform.forward * (Speed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Speed = 0;
+
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Vector3 pos = contact.point;
+
+        if(ImpactPrefab != null)
+        {
+            var impactVFX = Instantiate(ImpactPrefab, pos, rot);
+            Destroy(impactVFX, 5);
+        }
+
+        if(Trails.Count > 0)
+        {
+            for(int i = 0; i < Trails.Count; i++)
+            {
+                Trails[i].transform.parent = null;
+                var ps = Trails[i].GetComponent<ParticleSystem>();
+                if (ps != null)
+                {
+                    ps.Stop();
+                    Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                }
+            }
+        }
+
+        Destroy(gameObject);
+    }
+}
---
+++ characterShow/Assets/Scripts/Monster/Skills/SpawnFireBall.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFireBall : MonoBehaviour
+{
+    public GameObject Ball;
+    public Transform StartPoint, EndPoint;
+
+    private void Start()
+    {
+        var startPos = StartPoint.position;
+        GameObject ball = Instantiate(Ball, startPos, Quaternion.identity);
+
+        var endPos = EndPoint.position;
+
+        RotateTo(ball, endPos);
+    }
+
+    private void RotateTo(GameObject obj, Vector3 destination)
+    {
+        var direction = destination - obj.transform.position;
+        var rotation = Quaternion.LookRotation(direction);
+        obj.transform.localRotation = Quaternion.Lerp(obj.transform.rotation, rotation, 1);
+    }
+}
